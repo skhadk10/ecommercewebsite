451f5ddd06bcce10b07a0638bf259039af34faaf
skhadk10 <rickeykhd@gmail.com>
2022-08-21 15:19:45 +1000

    update category, delete  done

--- controller/category.js
+++ controller/category.js
@@ -1,16 +1,17 @@
 const Category = require("../models/category.js");
 const { errorHandler } = require("../helper/dbErrorHandler.js");
 
-exports.categoryById=(req,res,next,id)=>{
-    Category.findById(id).exec((err,category)=>{
-    if(err||!category){
-        return res.status(400).json({
-            error:"User not found"
-        })
+exports.categoryById = (req, res, next, id) => {
+  Category.findById(id).exec((err, category) => {
+    if (err || !category) {
+      return res.status(400).json({
+        error: "User not found",
+      });
     }
-    req.category=category;
-    next()
-})}
+    req.category = category;
+    next();
+  });
+};
 exports.create = (req, res) => {
   const category = new Category(req.body);
   category.save((err, data) => {
@@ -25,4 +26,43 @@ exports.create = (req, res) => {
 
 exports.read = (req, res) => {
   return res.json(req.category);
+};
+
+exports.update = (req, res) => {
+  // updating the data in the category.
+  const category = req.category;
+//   updating the list of name inside of category.
+// passing the new name in from frontend as req.body.name and passing inside the category and saving the new name
+  category.name = req.body.name;
+  category.save((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json(data);
+  });
+};
+exports.remove = (req, res) => {
+  const category = req.category;
+    category.remove((err, data) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      }
+      res.json({
+        message: "Category is deleted"
+      });
+    });
+  };
+// getting all the list of categories
+exports.list = (req, res) => {
+    Category.find().exec((err, data) => {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err),
+          });
+        }
+    })
 };
--- routes/category.js
+++ routes/category.js
@@ -2,13 +2,15 @@ const express = require("express");
 const { requireSignin, isAuth, isAdmin } = require("../controller/auth.js");
 const router = express.Router();
 
-const { create,categoryById, read } = require("../controller/category.js");
+const { create,categoryById, read ,update,remove,list} = require("../controller/category.js");
 const { userById } = require("../controller/user.js");
 
 router.get("/category/:categoryId", read);
 // admin can be able to create new category
 router.post("/category/create/:userId", requireSignin, isAuth, isAdmin, create);
-
+router.put("/category/:categoryId/:userId", requireSignin, isAuth, isAdmin, update);
+router.delete("/category/:categoryId/:userId", requireSignin, isAuth, isAdmin, remove);
+router.post("/categories", list);
 router.param("categoryId", categoryById);
 router.param("userId", userById);
 
