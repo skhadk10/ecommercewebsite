068afe9aabda6ae1a5e100b36827b3ce36bb9b96
skhadk10 <rickeykhd@gmail.com>
2022-08-25 21:27:17 +1000

    user read and update is done

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { requireSignin, isAuth, isAdmin } = require("../controller/auth.js");
 const router = express.Router();
 
-const { userById } = require("../controller/user.js");
+const { userById,read,update } = require("../controller/user.js");
 // we are using isAuth and isadmin 
 // isAuth use garnako karand k ho vanda user ko id ra authorizations token vayo vanye ni aaru user ni herna milxa tesai le is Auth use garera tyo id vako user lai matra access dena khojyeko 
 router.get("/secret/:userId", requireSignin,isAuth, isAdmin,  (req, res) => {
@@ -11,6 +11,10 @@ router.get("/secret/:userId", requireSignin,isAuth, isAdmin,  (req, res) => {
     user: req.profile,
   });
 });
+
+router.get("/user/:userId",  requireSignin,isAuth,read)
+router.put("/user/:userId",  requireSignin,isAuth,update)
+
 router.param("userId", userById);
 
 module.exports = router;
